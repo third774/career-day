40e138195ab3e2be34731e18c4657285e9952300
Kevin Kipp <kevin.kipp@gmail.com>
2018-05-08 21:50:54 -0500

    Add colors

---
+++ presentation/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+  miami: {
+    primary: "#FFBD39",
+    secondary: "#E61C5D",
+    tertiary: "#930077",
+    quarternary: "#3A0088"
+  },
+  speedRacer: {
+    primary: "#EAEAEA",
+    secondary: "#252A34",
+    tertiary: "#FF2E63",
+    quarternary: "#08D9D6"
+  }
+};
+
+export default colors;
