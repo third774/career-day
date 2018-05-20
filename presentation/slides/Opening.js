a0147355cb4b7f652bc2a4bba1f89f97e2d0ef69
Kevin Kipp <kevin.kipp@gmail.com>
2018-05-20 18:18:43 -0500

    Remove transitions from opening slides

--- presentation/slides/Opening.js
+++ presentation/slides/Opening.js
@@ -1,24 +1,20 @@
 import React from "react";
-import { Appear, Heading, Slide, Text } from "spectacle";
+import { Heading, Slide, Text } from "spectacle";
 import images from "../images";
 
 export default (
   <Slide transition={["fade"]} bgImage={images.code} bgDarken={0.7}>
-    <Appear>
-      <Heading size={1} fit caps lineHeight={1} textColor="primary">
-        Software Engineering
-      </Heading>
-    </Appear>
-    <Appear>
-      <Text
-        textFont="secondary"
-        margin="10px 0 0"
-        textColor="tertiary"
-        size={1}
-        bold
-      >
-        having fun solving problems
-      </Text>
-    </Appear>
+    <Heading size={1} fit caps lineHeight={1} textColor="primary">
+      Software Engineering
+    </Heading>
+    <Text
+      textFont="secondary"
+      margin="10px 0 0"
+      textColor="tertiary"
+      size={1}
+      bold
+    >
+      having fun solving problems
+    </Text>
   </Slide>
 );
